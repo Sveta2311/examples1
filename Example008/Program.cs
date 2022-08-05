78a7731eb801b83a529eddb08ff91e98c8f69367
Sveta <margaritka2132@mail.ru>
2022-08-05 22:43:07 +0300

    Example 008

---
+++ Example008/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("number_A");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+Console.Write("number_B");
+string number_Bs = Console.ReadLine() ??"";
+int number_Bi;
+if (int.TryParse(number_As, out number_Ai))
+{
+   if (int.TryParse(number_Bs, out number_Bi))
+   {
+      if (number_Ai == number_Bi * number_Bi)
+      {
+         Console.WriteLine("Yahoo");
+      }
+      else
+      {
+         Console.WriteLine("No");
+      }  
+   }
+   else
+   {
+      Console.WriteLine("Error");
+   }
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+
