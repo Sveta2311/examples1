3ab3b6db54688c3f56d8987a51360be11722b09b
Sveta <margaritka2132@mail.ru>
2022-08-06 21:18:14 +0300

    Example home 1

---
+++ Example_home1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите первое число");
+string number_As = Console.ReadLine() ??"";
+double number_Ai;
+Console.WriteLine("Введите второе число");
+string number_Bs = Console.ReadLine() ??"";
+double number_Bi;
+if (double.TryParse(number_As, out number_Ai))
+{
+   if (double.TryParse(number_Bs, out number_Bi))
+   {
+      if (number_Ai == number_Bi)
+      {
+         Console.WriteLine("Числа совпадают: " + number_As);
+      }
+      else
+      {
+         if (number_Ai < number_Bi) 
+         {
+            Console.WriteLine("max = " + number_Bs);
+            Console.WriteLine("min = " + number_As);
+         } 
+         else 
+         {
+            Console.WriteLine("min = " + number_Bs);
+            Console.WriteLine("max = " + number_As);
+         }   
+      } 
+   }
+   else
+   {
+      Console.WriteLine("Ошибка ввода");
+   }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
