8b831aeaec525cd69db9104ed2a14455c2c81060
Sveta <margaritka2132@mail.ru>
2022-08-07 19:22:00 +0300

    new changes, removed nested if-else

--- Example_home2/Program.cs
+++ Example_home2/Program.cs
@@ -8,32 +8,18 @@ Console.WriteLine("Ведите третье число");
 string number_Cs = Console.ReadLine() ??"";
 double number_Ci;
 double max;
-if (double.TryParse(number_As, out number_Ai))
+
+if (!double.TryParse(number_As, out number_Ai) || !double.TryParse(number_Bs, out number_Bi) || !double.TryParse(number_Cs, out number_Ci))
 {
-  if (double.TryParse(number_Bs, out number_Bi))
-   {
-      if (double.TryParse(number_Cs, out number_Ci))
-      {
-       max = number_Ai;
-       if (number_Bi> max ) max = number_Bi;
-       if (number_Ci> max ) max = number_Ci;
-       Console.Write("max = ");
-       Console.WriteLine(max);
-      }
-      else 
-      {
-      Console.WriteLine("Ошибка ввода");
-      } 
-    } 
-    else
-    {
-    Console.WriteLine("Ошибка ввода");
-    } 
-  }
-  else
-  {
   Console.WriteLine("Ошибка ввода");
+  return;
 }
+
+max = number_Ai;
+if (number_Bi> max ) max = number_Bi;
+if (number_Ci> max ) max = number_Ci;
+Console.Write("max = ");
+Console.WriteLine(max);
 
 
 
