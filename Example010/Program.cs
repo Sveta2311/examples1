34440fd91a91b6d4cdd12fee258ff6bf19b61a1e
Sveta <margaritka2132@mail.ru>
2022-08-09 22:29:24 +0300

    added Example 010, Example 011

---
+++ Example010/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a3 = num % 10;
+string s1 = a1.ToString();
+string s3 = a3.ToString();
+string result = s1 + s3;
+Console.WriteLine(num);
+Console.Write(a1);
+Console.WriteLine(a3);
---
+++ Example011/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = new Random().Next(10, 100);
+int num2 = new Random().Next(1, 10);
+int result = num % num2;
+if (result == 0)
+    Console.WriteLine ($"{num} кратно {num2}");
+else 
+    Console.WriteLine ($"{num} не кратно {num2}, остаток {result}");
+
