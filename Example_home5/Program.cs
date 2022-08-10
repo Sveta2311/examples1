6ba0ef467dd35ab46f1f8187c6304ce76c28194b
Sveta <margaritka2132@mail.ru>
2022-08-10 17:08:22 +0300

    added Example home 5, Example home 6, Example home 7

---
+++ Example_home5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+if (!int.TryParse(number_As, out number_Ai) || number_As.Length != 3)
+{
+     Console.WriteLine("Ошибка ввода");
+     return;
+}
+Console.WriteLine("Вторая цифра введенного трехзначного числа:");
+Console.WriteLine(number_As.Substring(1,1));
---
+++ Example_home6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+if (!int.TryParse(number_As, out number_Ai))
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+if (number_As.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет в веденном числе");
+    return;
+}
+Console.WriteLine("Третья цифра введенного числа:");
+Console.WriteLine(number_As.Substring(2,1));
