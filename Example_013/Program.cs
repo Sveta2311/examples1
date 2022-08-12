8076566ce80e9015ceaae24efb3a74da46180a7d
Sveta <margaritka2132@mail.ru>
2022-08-12 23:26:13 +0300

    Example 013

---
+++ Example_013/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+static int inputCoord()
+{
+   try
+   {
+     return int.Parse(Console.ReadLine());
+   }
+   catch (Exception exc)
+   {
+   Console.WriteLine(exc.Message);
+   }
+   return 0;
+}
+private static void Main(string[] args)
+{
+   Console.Clear();
+     
+     Console.WriteLine("Ведите x1: ");
+     double x1 = inputCoord();
+
+     Console.WriteLine("Ведите y1: ");
+     double y1 = inputCoord();
+
+     Console.WriteLine("Ведите x2: ");
+     double x2 = inputCoord();
+
+     Console.WriteLine("Ведите y2: ");
+     double y2 = inputCoord();
+
+     double res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1-y2), 2));
+     Console.Write(res);
+}
+}
