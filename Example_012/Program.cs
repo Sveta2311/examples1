4532a54b680935cf96eca334e53a8164e87806f0
Sveta <margaritka2132@mail.ru>
2022-08-12 22:23:33 +0300

    Example 012

---
+++ Example_012/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int q = 0;
+try 
+{
+    q = int.Parse(Console.ReadLine());
+}
+catch (Exception exc)
+{
+   Console.WriteLine(exc.Message); 
+}
+switch (q)
+{
+    case 1:
+    Console.WriteLine("x > 0 и y > 0");
+    break;
+    case 2:
+    Console.WriteLine("x < 0 и y > 0");
+    break;
+    case 3:
+    Console.WriteLine("x < 0 и y < 0");
+    break;
+    case 4:
+    Console.WriteLine("x > 0 и y < 0");
+    break;
+    default:
+    Console.WriteLine("Неверно введен номер четверти");
+    break;
+}
