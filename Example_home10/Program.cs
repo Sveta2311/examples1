56f4cdd183d625c7a7a0daebaa1193f5bd5da14d
Sveta <margaritka2132@mail.ru>
2022-08-14 16:32:01 +0300

    added Example 10

---
+++ Example_home10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+double cube;
+if (!int.TryParse(number_As, out number_Ai) || !(number_Ai > 0))
+{
+     Console.WriteLine("Ошибка ввода");
+     return;
+}
+Console.WriteLine("Все числа в кубе от 1 до " + number_As + ":");
+for (int i = 1; i<= number_Ai; i++)
+{
+     cube = Math.Pow(i, 3);
+     Console.WriteLine(cube);
+}       
