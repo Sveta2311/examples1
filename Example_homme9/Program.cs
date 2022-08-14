20bf6b7f5716ca5adb5a594b684253eb77dc189b
Sveta <margaritka2132@mail.ru>
2022-08-14 16:30:50 +0300

    added Example 9

---
+++ Example_homme9/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+static int inputCoord()
+{
+   try
+   {
+     return int.Parse(Console.ReadLine());
+   }
+   catch (Exception exc)
+   {
+   Console.WriteLine(exc.Message);
+   }
+   return 0;
+}
+private static void Main(string[] args)
+{
+   Console.Clear();
+     
+     Console.WriteLine("Ведите x1: ");
+     double x1 = inputCoord();
+
+     Console.WriteLine("Ведите y1: ");
+     double y1 = inputCoord();
+
+     Console.WriteLine("Ведите z1: ");
+     double z1 = inputCoord();
+
+     Console.WriteLine("Ведите x2: ");
+     double x2 = inputCoord();
+
+     Console.WriteLine("Ведите y2: ");
+     double y2 = inputCoord();
+
+     Console.WriteLine("Ведите z2: ");
+     double z2 = inputCoord();
+
+     double res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+     Console.Write(Math.Round(res, 2));
+}
+}
