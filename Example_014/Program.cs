b184dafc1a4d5f24695e3a776c121e469c8bf59c
Sveta <margaritka2132@mail.ru>
2022-08-16 23:46:53 +0300

    added Example 014

---
+++ Example_014/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountNumber(int num)
+{
+    int i=0;
+    while(num > 0)
+    {
+        num/=10;
+        i++;
+    }
+    return i;
+}
+
+int number;
+Console.WriteLine("Ведите любое число: ");
+number = int.Parse(Console.ReadLine()?? "");
+number = CountNumber(number);
+
+Console.Write($"Число состоит из {number} цифр");
