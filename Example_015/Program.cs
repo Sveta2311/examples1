e1f10d0726113ec90ff9624ed503eb825a9cfe42
Sveta <margaritka2132@mail.ru>
2022-08-16 23:47:20 +0300

    added Example 015

---
+++ Example_015/Program.cs
@@ -0,0 +1,16 @@
+﻿int CalculatorFactorial(int num)
+{
+    int result = 1, i=1;
+    while (i <= num)
+    {
+        result*=i;
+        i++;
+    }
+    return result;
+}
+
+Console.WriteLine("Ведите любое число: ");
+int userNumber = int.Parse(Console.ReadLine()?? "");
+int factorial = CalculatorFactorial(userNumber);
+
+Console.Write($"Факториал числа {userNumber} = {factorial}");
