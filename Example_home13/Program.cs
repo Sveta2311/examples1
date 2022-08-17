09e9baddf34dc8618d49cfc51f785502d2fe87f5
Sveta <margaritka2132@mail.ru>
2022-08-17 17:10:21 +0300

    added Example home 13

---
+++ Example_home13/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myArray = new string[8];
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write($"\nВведите элемент массива под индексом {i}: ");
+            myArray[i] = Console.ReadLine();
+        }
+    Console.WriteLine("\nВывод массива: ");
+    Console.WriteLine(String.Join(", ", myArray));
+    }
+}
