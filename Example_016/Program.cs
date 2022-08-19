5a55e205d8a8a6aa6e82efe968362534bca1fc2e
Sveta <margaritka2132@mail.ru>
2022-08-19 22:23:35 +0300

    added Example 016

---
+++ Example_016/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите колличество элементов массива:");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = GetArray(size);
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] *= (-1);
+        }
+        Console.WriteLine("\nВывод массива: ");
+        Console.WriteLine(String.Join(", ", myArray));
+    }
+
+    static int[] GetArray(int length)
+    {
+        int[] myArray = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write($"\nВведите элемент массива под индексом {i}: ");
+            myArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return myArray;
+    }
+}
