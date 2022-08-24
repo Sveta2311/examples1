addc3a5788d377afb6003ed7059439b348c77e3b
Sveta <margaritka2132@mail.ru>
2022-08-24 18:31:34 +0300

    added Example home 021

---
+++ Example_home021/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+double k1 = GetCoords("Введите число k1: ");
+double k2 = GetCoords("Введите число k2: ");
+double b1 = GetCoords("Введите число b1: ");
+double b2 = GetCoords("Введите число b2: ");
+
+double GetCoords(string message)
+{
+   while(true)
+      {
+      Console.Write(message);
+      double value;
+      try 
+      {
+         value = Convert.ToDouble(Console.ReadLine());
+         return value;
+      }
+        catch 
+      {
+         Console.WriteLine("Вы ввели не число!");
+      }
+    }
+}
+
+if (k1 == k2)
+{
+   Console.WriteLine("Прямые параллельны!");
+   return;
+}
+else 
+{
+   double x = -(b1 - b2) / (k1 - k2);
+   double y = k1 * x + b1;
+ 
+   x = Math.Round(x, 2);
+   y = Math.Round(y, 2);
+ 
+   Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+} 
