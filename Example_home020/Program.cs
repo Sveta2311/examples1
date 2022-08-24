2d486b67fc40239643aa76224fc16cc2bbaabb77
Sveta <margaritka2132@mail.ru>
2022-08-24 18:31:08 +0300

    added Example home 020

---
+++ Example_home020/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int result = 0;
+double number;
+
+while(true)
+    {
+        Console.Write("Введите число (Для завершения программы нажмите Q): ");
+        string value = Console.ReadLine();
+        try 
+        {
+            number = Convert.ToDouble(value);
+            if (number > 0 ) result++;
+        }
+        catch 
+        {
+            if(value == "Q")
+            {
+                Console.WriteLine("Количество чисел больше 0: " + result);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число!");
+            }
+        }
+    }
