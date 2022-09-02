95861419792a4824e4b6791926dcbc567d8c76d1
Sveta <margaritka2132@mail.ru>
2022-09-02 23:34:58 +0300

    added Example home 032

---
+++ Example_home032/Program.cs
@@ -0,0 +1,29 @@
+﻿int m = GetNumberFromUser("Введите первое целое положительное число: ", "Ошибка ввода!");
+int n = GetNumberFromUser("Введите второе целое положительное число: ", "Ошибка ввода!");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userNumber;
+        bool isCorrect = int.TryParse(Console.ReadLine(), out userNumber);
+        if (isCorrect && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int test = A(n, m);
+Console.WriteLine("Значение функции Аккермана: ");
+Console.WriteLine(test);        
+static int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return A(n - 1, 1);
+    if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+    return A(n,m);
+}
+
+
+
