c066b9a2dfecf0e7400051cff8c6b4bd9dcedfbc
Sveta <margaritka2132@mail.ru>
2022-09-02 21:54:46 +0300

    added Example 024

---
+++ Example_024/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int num = GetNumberFromUser("Введите целое положительное число: ", "Ошибка ввода!");
+Console.WriteLine("Сумма цифр в введенном числе:");
+Console.WriteLine($"\t{num} -> {GetSumDigits(num)}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        int userNumber;
+        bool isCorrect = int.TryParse(Console.ReadLine(), out userNumber);
+        if(isCorrect && userNumber >=0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int GetSumDigits(int number)
+{
+    if (number == 0)
+    return 0;
+    return number % 10 + GetSumDigits(number / 10);
+}
