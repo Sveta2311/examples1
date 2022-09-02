1b561e0bc90eede195175f1485eb6ab06ec8b3ef
Sveta <margaritka2132@mail.ru>
2022-09-02 21:54:00 +0300

    added Example 022

---
+++ Example_022/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число => 1: ");
+string number_As = Console.ReadLine() ?? "";
+int number_Ai;
+if (!int.TryParse(number_As, out number_Ai))
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+if (number_Ai < 1)
+{
+    Console.WriteLine("Введенное число меньше 1");
+    return;
+}
+
+PrintNaturalsNambers(1, number_Ai);
+
+string PrintNaturalsNambers(int start, int end)
+{
+    if (start == end + 1)
+        return start.ToString();
+    else
+    {
+        if (start == end)
+            Console.Write($"{start}");
+        else
+            Console.Write($"{start}, ");
+        return PrintNaturalsNambers(start + 1, end);
+    }
+}
+
