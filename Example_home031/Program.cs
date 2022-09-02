a4c75a3e1485ef2e51450703e69ea651a9fe42fa
Sveta <margaritka2132@mail.ru>
2022-09-02 23:05:57 +0300

    added Example home031

---
+++ Example_home031/Program.cs
@@ -0,0 +1,39 @@
+﻿int num = GetNumberFromUser("Введите первое целое положительное число: ", "Ошибка ввода!");
+int num2 = GetNumberFromUser("Введите второе целое положительное число: ", "Ошибка ввода!");
+int sum = 0;
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userNumber;
+        bool isCorrect = int.TryParse(Console.ReadLine(), out userNumber);
+        if (isCorrect && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+if (num > num2)
+{
+    Console.Write("Ошибка ввода, второе введенное число должно быть больше первого!");
+    return;
+}
+else
+    sum = SumNaturalsNambers(num, num2, 0);
+Console.WriteLine($"Сумма натуральных чисел в веденном прмежутке: {sum}");
+
+int SumNaturalsNambers(int start, int end, int sum)
+{
+    if (start == end)
+    {
+        return sum += start;
+    }
+    else
+    {
+        sum += start;
+        return SumNaturalsNambers(start + 1, end, sum);
+    }
+
+}
