4e54f64c8c7607e7d54f4fc6cfa84aa2dd37f33a
Sveta <margaritka2132@mail.ru>
2022-09-02 22:57:29 +0300

    changes made Example 025

--- Example_025/Program.cs
+++ Example_025/Program.cs
@@ -1,31 +1,4 @@
-﻿// Console.Clear();
-// Console.WriteLine("Введите первое число: ");
-// string number_As = Console.ReadLine() ??"";
-// double number_Ai;
-// Console.WriteLine("Введите второе натуральное (положительное и больше 0) число: ");
-// string number_Bs = Console.ReadLine() ??"";
-// int number_Bi;
-// double stepen;
-// if (!double.TryParse(number_As, out number_Ai))
-// {
-//      Console.WriteLine("Ошибка ввода, повторите ввод!");
-//      return;
-// }
-
-// if (!int.TryParse(number_Bs, out number_Bi) || number_Bi < 0)
-// {
-//      Console.WriteLine("Ошибка ввода, возможно, вы ввели не целое или отрицательное число, повторите ввод!");
-//      return;
-// }
-
-// Console.WriteLine("Первое введенное число, возведенное в натуральную степень второго введенного числа: ");
-
-// {
-//      stepen = Math.Pow(number_Ai, number_Bi);
-//      Console.WriteLine(stepen);
-// }  
-
-Console.Clear();
+﻿Console.Clear();
 
 int num = GetNumberFromUser("Введите целое положительное число: ", "Ошибка ввода!");
 int num2 = GetNumberFromUser("Введите целое положительное число: ", "Ошибка ввода!");
